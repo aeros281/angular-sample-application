8d2602b869eb2a8bd92875628d50e324e93e72d2
Anh Le Tuan Tran <tranletuananh1992@gmail.com>
2020-02-13 21:37:32 +0700

    Refactor route configuration to AppRoutingModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StaffListComponent } from './staff-list/staff-list.component';
+import { RuleListComponent } from './rule-list/rule-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+const routes: Routes = [
+  { path: 'staff-management', component: StaffListComponent },
+  { path: 'rule-management', component: RuleListComponent },
 
-const routes: Routes = [];
+  { path: '', redirectTo: '/staff-management', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(
+      routes,
+      { enableTracing: true }
+     )
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,8 @@ import { AppComponent } from './app.component';
 import { StaffListComponent } from './staff-list/staff-list.component';
 import { RuleListComponent } from './rule-list/rule-list.component';
 import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
-const appRoutes: Routes = [
-  { path: 'staff-management', component: StaffListComponent },
-  { path: 'rule-management', component: RuleListComponent },
-
-  { path: '', redirectTo: '/staff-management', pathMatch: 'full' },
-  { path: '**', component: PageNotFoundComponent }
-];
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -26,10 +18,7 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true }
-    )
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
