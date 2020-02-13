8ad5a58f53e889031a58167ac0581090a67140d9
Anh Le Tuan Tran <tranletuananh1992@gmail.com>
2020-02-13 21:27:51 +0700

    Add multiple components and routing configurations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StaffListComponent } from './staff-list/staff-list.component';
 import { RuleListComponent } from './rule-list/rule-list.component';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'staff-management', component: StaffListComponent },
+  { path: 'rule-management', component: RuleListComponent },
+
+  { path: '', redirectTo: '/staff-management', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     StaffListComponent,
-    RuleListComponent
+    RuleListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
